a9367a6ff65ef73359651a43382015c0505fbd55
PavPar <ParamonPav@yandex.ru>
2020-05-21 17:23:02 +0300

    Final version of Thread buttons

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace LAB_9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.ForestGreen;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(68, 30);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add BTN";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(86, 19);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(227, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB_9
+{
+
+    public partial class Form1 : Form
+    {
+        int delay = 5;
+        List<Thread> threads;
+        List<Button> btns;
+        public Form1()
+        {
+            InitializeComponent();
+            threads = new List<Thread>();
+            btns = new List<Button>();
+            label1.Text = "Текущее кол-во потоков " + Process.GetCurrentProcess().Threads.Count.ToString();
+            label2.Text = "Начальное кол-во потоков " + Process.GetCurrentProcess().Threads.Count.ToString();
+            label3.Text = "Кол-во созданных кнопок " + 0;
+
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            threads.Add(addButton());
+            threads.Last().Start();
+            label1.Text = "Текущее кол-во потоков " + Process.GetCurrentProcess().Threads.Count.ToString();
+            label3.Text = "Кол-во созданных кнопок " + btns.Count;
+        }
+
+        private int counter = 2;
+        public void moveBtn(object sender, MouseEventArgs e)
+        {
+            Button btn = sender as Button;
+            Point pos = btn.PointToClient(Cursor.Position);
+            Point center = new Point(((pos.X - (btn.Size.Width / 2)) + Math.Sign(pos.X - (btn.Size.Width / 2))), ((pos.Y - (btn.Size.Height / 2)) + Math.Sign(pos.Y - (btn.Size.Height / 2))));
+            const int offset = 5;
+            if (pos.X < btn.Width + offset && pos.X > 0 - offset && pos.Y > 0 - offset && pos.Y < btn.Height + offset)
+            {
+
+
+                if ((float)Math.Abs(center.X) / (btn.Size.Width / 2) > (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+                {
+                    btn.Location = new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y);
+                }
+                else
+                {
+                    if ((float)Math.Abs(center.X) / (btn.Size.Width / 2) == (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+                    {
+                        btn.Location = new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2));
+                    }
+                    else
+                    {
+                        btn.Location = new Point(btn.Location.X, btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2));
+                    }
+                }
+
+                Form ActiveForm = (sender as Button).Parent as Form;
+                try
+                {
+                    if (btn.Location.X > ActiveForm.Size.Width - 2 * btn.Size.Width)
+                    {
+                        btn.Location = new Point(ActiveForm.Size.Width - 2 * btn.Size.Width, btn.Location.Y);
+                    }
+                    if (btn.Location.X < 0 + btn.Size.Width)
+                    {
+                        btn.Location = new Point(0 + btn.Size.Width, btn.Location.Y);
+                    }
+                    if (btn.Location.Y > ActiveForm.Size.Height - 2 * btn.Size.Height)
+                    {
+                        btn.Location = new Point(btn.Location.X, ActiveForm.Size.Height - 2 * btn.Size.Height);
+                    }
+                    if (btn.Location.Y < 0 + btn.Size.Height)
+                    {
+                        btn.Location = new Point(btn.Location.X, 0 + btn.Size.Height);
+                    }
+                }
+                catch
+                {
+                    //idk
+                }
+                label1.Text = Process.GetCurrentProcess().Threads.Count.ToString();
+            }
+
+        }
+        public Thread addButton()
+        {
+            Thread thr = new Thread(new ThreadStart(createButton));
+            return thr;
+        }
+
+        public delegate Point PointDelegate(Button btn);
+        public Point GetPoint(Button btn)
+        {
+            return btn.PointToClient(Cursor.Position);
+        }
+
+        public delegate void SetPointDelegate(Button btn, Point pos);
+        public void SetPoint(Button btn, Point pos)
+        {
+            btn.Location = pos;
+        }
+
+        public void createButton()
+        {
+            Button button1 = new System.Windows.Forms.Button();
+            var nDeleg = new PointDelegate(GetPoint);
+            var nDelegSet = new SetPointDelegate(SetPoint);
+            Random rnd = new Random();
+            button1.Name = "button_" + (++counter);
+            int size = rnd.Next(15, 100);
+            button1.Size = new System.Drawing.Size(size, size);
+            Point btnpos = new Point(rnd.Next(0 + 2 * button1.Size.Width, ActiveForm.Size.Width - 2 * button1.Size.Width), rnd.Next(0 + 2 * button1.Size.Height, ActiveForm.Size.Height - 2 * button1.Size.Height));
+            button1.Location = btnpos;
+            button1.TabIndex = 0;
+            button1.BackColor = Color.FromArgb(rnd.Next(256), rnd.Next(256), rnd.Next(256));
+            button1.UseVisualStyleBackColor = false; //!!!
+            button1.Text = "";
+            this.Invoke((MethodInvoker)(() => Controls.Add(button1)));
+            //button1.MouseMove += new System.Windows.Forms.MouseEventHandler(moveBtn);
+            //button1.Click += new System.EventHandler(createButton);
+            //while (true) { Thread.Sleep(1000); }
+            btns.Add(button1);
+            while (true)
+            {
+                try
+                {
+                    Button btn = button1;
+                    //Point pos = btn.PointToClient(Cursor.Position);
+                    Point pos = (Point)Invoke(nDeleg, btn);
+                    Point center = new Point(((pos.X - (btn.Size.Width / 2)) + Math.Sign(pos.X - (btn.Size.Width / 2))), ((pos.Y - (btn.Size.Height / 2)) + Math.Sign(pos.Y - (btn.Size.Height / 2))));
+                    const int offset = 0;
+                    if (pos.X < btn.Width + offset && pos.X > 0 - offset && pos.Y > 0 - offset && pos.Y < btn.Height + offset)
+                    {
+
+
+                        if ((float)Math.Abs(center.X) / (btn.Size.Width / 2) > (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+                        {
+                            Invoke(nDelegSet, btn, new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y));
+                        }
+                        else
+                        {
+                            if ((float)Math.Abs(center.X) / (btn.Size.Width / 2) == (float)Math.Abs(center.Y) / (btn.Size.Height / 2))
+                            {
+                                Invoke(nDelegSet, btn, new Point(btn.Location.X + center.X + Math.Sign(-center.X) * (btn.Size.Width / 2), btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2)));
+                            }
+                            else
+                            {
+                                Invoke(nDelegSet, btn, new Point(btn.Location.X, btn.Location.Y + center.Y + Math.Sign(-center.Y) * (btn.Size.Height / 2)));
+                            }
+                        }
+
+                        Form ActiveForm = button1.Parent as Form;
+                        try
+                        {
+                            if (btn.Location.X > ActiveForm.Size.Width - 2 * btn.Size.Width)
+                            {
+                                Invoke(nDelegSet, btn, new Point(ActiveForm.Size.Width - 2 * btn.Size.Width, btn.Location.Y));
+                            }
+                            if (btn.Location.X < 0 + btn.Size.Width)
+                            {
+                                Invoke(nDelegSet, btn, new Point(0 + btn.Size.Width, btn.Location.Y));
+                            }
+                            if (btn.Location.Y > ActiveForm.Size.Height - 2 * btn.Size.Height)
+                            {
+                                Invoke(nDelegSet, btn, new Point(btn.Location.X, ActiveForm.Size.Height - 2 * btn.Size.Height));
+                            }
+                            if (btn.Location.Y < 0 + btn.Size.Height)
+                            {
+                                Invoke(nDelegSet, btn, new Point(btn.Location.X, 0 + btn.Size.Height));
+                            }
+                        }
+                        catch
+                        {
+                            //idk
+                        }
+                    }
+                    Thread.Sleep(delay);
+                }
+                catch
+                {
+
+                }
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (Thread thr in threads)
+            {
+                thr.Abort();
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            delay = (int)(sender as NumericUpDown).Value;
+        }
+    }
+
+}
